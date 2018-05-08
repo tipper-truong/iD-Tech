ede6e55150e0fb9decb2b3da206bc3c6ce905c20
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-05-08 15:03:51 -0700

    able to detect objects in images

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug import secure_filename
+import subprocess
+import sys
 
 UPLOAD_FOLDER = os.path.join('static', 'images')
-ALLOWED_EXTENSIONS = set(['jpeg', 'png', 'jpg', 'JPG', 'PNG', 'JPEG'])
+PREDICT_FOLDER = os.path.join('darknet')
+ALLOWED_EXTENSIONS = set(['jpeg', 'png', 'jpg', 'JPG', 'PNG', 'JPEG', "MOV", "m4v", "mov"])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['PREDICT_FOLDER'] = PREDICT_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -28,6 +32,19 @@ def upload():
 
 @app.route("/gallery")
 def images():
+    if(request.args.get("image")):
+
+        #cmd = "./darknet detect cfg/yolov3.cfg yolov3.weights {}".format("../" + app.config['UPLOAD_FOLDER'] + "/" + request.args.get("image")) # image
+        cmd =  "./darknet detector demo cfg/coco.data cfg/yolov3.cfg yolov3.weights {}".format("../" + app.config['UPLOAD_FOLDER'] + "/" + request.args.get("image")) # video
+        print(cmd)
+        p = subprocess.Popen(['(cd darknet/;{})'.format(cmd)], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout = []
+        while True:
+            line = p.stdout.readline()
+            stdout.append(line)
+            print(line),
+            if line == '' and p.poll() != None:
+                return send_from_directory(app.config['PREDICT_FOLDER'], 'predictions.png')
     images = os.listdir(app.config['UPLOAD_FOLDER'])
     return render_template("gallery.html", images=images)
 
